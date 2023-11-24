6b05e692f77eb05b33a7ff5f2964c558af8a3ee5
Branden108 <bsi@hawk.iit.edu>
2023-11-24 12:24:47 -0600

    Added media query for songs page

---
+++ js/favoritesong.js
@@ -0,0 +1,4 @@
+function submitFavoriteSong() {
+    var favoriteSong = document.getElementById('favorite-song-input').value;
+    alert('Thank you for sharing! Your favorite song is: ' + favoriteSong);
+}
